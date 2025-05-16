4a254fc3382d480b9df585ecef755df7a12867f2
asarria48 <92031732+asarria48@users.noreply.github.com>
2025-05-16 04:09:01 +0000

    calculado el espesor de la laminita para el oro, añadí más cifras significativas

--- Problemas1/punto3/p3laminitas.py
+++ Problemas1/punto3/p3laminitas.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
-rho = 10.49  # Density [g/cm^3]
+rho = 19.32  # Density [g/cm^3]
 
 
-E = 0.75                    # Energy found in the previous plot [MeV]
-element = 'Ag'           
-color = 'palevioletred'          
+E = 0.39                    # Energy found in the previous plot [MeV]
+element = 'Au'           
+color = 'darkorange'          
 
 # NIST data
-data = np.loadtxt('silver.txt')
+data = np.loadtxt('gold.txt')
 x = data[:, 0]              # Kinetic energy [MeV]
 y = data[:, 1] / rho        # CSDA range divided into density [g/cm**2] / [g/cm**3] = [cm]
 
@@ -19,7 +19,7 @@ log_interp = interp1d(np.log10(x), np.log10(y), kind='linear', bounds_error=Fals
 log_y_val = log_interp(np.log10(E))
 R_equiv = 10**log_y_val  
 
-R_microns = R_equiv * 1e4
+R_microns = R_equiv * 1e4   # turn centimetres into micrometres
 
 
 plt.figure(figsize=(10, 8))
@@ -40,18 +40,18 @@ plt.ylim(np.min(y), np.max(y))
 
 plt.xlabel('E [MeV]')
 plt.ylabel('R/$\\rho$ [cm]')
-plt.title('Rango de las partículas $\\alpha$ en Ag')
+plt.title('Rango de las partículas $\\alpha$ en Au')
 
 
 plt.legend([
     plt.Line2D([], [], color='black'),
     plt.Line2D([], [], color=color, linestyle='--')
 ], [
-    r'$\alpha$ + Ag',
-    f'E = {E:.2f} MeV, R = {R_microns:.0f} µm' 
+    r'$\alpha$ + Au',
+    f'E = {E:.2f} MeV, R = {R_microns:.2f} µm' 
 ], loc='best')
 
 plt.grid(True, which="both",color='lightgray', ls="-", alpha=0.5)
 plt.tight_layout()
-plt.savefig('silver.png', dpi=300)
+plt.savefig('gold.png', dpi=300)
 plt.show()
